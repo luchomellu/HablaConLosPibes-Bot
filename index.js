5b85baa9f1b280a5fb38ca127c7ca631802de2ae
Luciano Melluso <luchomellu@live.com.ar>
2022-02-06 03:48:18 -0300

    Creo el bot

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents:[
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if(message.content == "hola"){
+        message.reply("Los pibes dicen: Hola!")
+    }
+})
+client.login(process.env.TOKEN)
